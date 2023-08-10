f63d95fe908a525df8de06452cd9b0d18c1e6ca7
Azhr Saveland <azhr.ciel@gmail.com>
2023-08-10 11:20:42 -0700

    minimalized if statements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import time
-import requests # pip install requests
-from datetime import datetime, timedelta # pip install datetime
+import requests
+from datetime import datetime, timedelta
 
 YOUR_DISCORD_WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 TARGET_USERNAME = os.getenv("TARGET_USERNAME")
@@ -9,31 +9,26 @@ TARGET_USERNAME = os.getenv("TARGET_USERNAME")
 def get_repositories(user):
     url = f"https://api.github.com/users/{user}/repos"
     headers = {'Authorization': f'token {os.getenv("GITHUB_TOKEN")}'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:  # Check if the API response indicates an error
-        print(f"Error {response.status_code}: {response.json().get('message')}")
-        return {}
-    else:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
         return {repo['name']: repo['created_at'] for repo in response.json()}
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        return {}
 
 def less_than_a_day_old(timestamp):
     created_at = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
     return datetime.now() - created_at < timedelta(days=1)
 
 def notify_discord(deleted_repos):
-    url = YOUR_DISCORD_WEBHOOK_URL
-
     for repo, timestamp in deleted_repos.items():
         data = {
             'content': f"Repository {repo} that was created at {timestamp} has been deleted."
         }
-        result = requests.post(url, json=data)
-
-        if result.status_code == 200:
-            print('Notification sent to Discord.')
-        else:
-            print(f'Failed to send notification to Discord, status code: {result.status_code}')
+        result = requests.post(YOUR_DISCORD_WEBHOOK_URL, json=data)
+        message = 'Notification sent to Discord.' if result.status_code == 200 else f'Failed to send notification to Discord, status code: {result.status_code}'
+        print(message)
 
 def main():
     user = TARGET_USERNAME
